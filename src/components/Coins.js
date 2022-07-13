dd9f8d9a1e306504e37cef114cef4623fa2af60f
Gurprem Bhullar <gurprembhullar@Gurprems-Air.home>
2022-07-13 13:45:25 -0400

    found out how to add coin id functionality

--- src/components/Coins.js
+++ src/components/Coins.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import CoinItem from './CoinItem'
+import Coin from '../routes/Coin'
+import {Link} from'react-router-dom'
 
 const Coins = (props) => {
   return (
@@ -16,7 +18,10 @@ const Coins = (props) => {
 
             {props.coins.map(coins => {
                 return (
-                    <CoinItem coins={coins} key={coins.id} />
+                    <Link to={`/coin/${coins.id}`} element={<Coin />} key={coins.id}>
+                        <CoinItem coins={coins}  />
+                    </Link>
+                        
                 )
             })}
 
