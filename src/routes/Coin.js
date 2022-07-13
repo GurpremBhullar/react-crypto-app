5dd36fdf4523c03ef2805dcc6eb5df906a57ddd1
Gurprem Bhullar <gurprembhullar@Gurprems-Air.home>
2022-07-13 15:54:37 -0400

    added second api functionality

--- src/routes/Coin.js
+++ src/routes/Coin.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, {useState, useEffect } from 'react'
+import axios from 'axios'
+
 
 const Coin = () => {
+
+  const [coin, setCoin] = useState({})
+
+  const url = `https://api.coingecko.com/api/v3/coins/bitcoin`
+
+  useEffect(() => {
+    axios.get(url).then((res) => {
+      setCoin(res.data)
+    }).catch((error) => {
+      console.log(error)
+    })
+  }, [])
+
   return (
     <div>
-      <h1>coin</h1>
+      <h1>{coin.id}</h1>
     </div>
   )
 }
